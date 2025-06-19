804dc6e8f40c0708df1f410c7ba5793da404c928
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2025-06-19 17:58:30 +0200

    FMOD & Misc. Update
    Decoupled Update.
    - Integrated FMOD Plugin and configured its settings
    - Created several different options for testing purposes
    - Added test implementation for new BCP Splash Screen

---
+++ Assets/Lukas/Scripts/Audio/Soundtesting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FMODUnity;
+using UnityEngine;
+
+namespace Scripts.Audio
+{
+    public class Soundtesting : MonoBehaviour
+    {
+        StudioEventEmitter emitter;
+
+        void Awake()
+        {
+            emitter = GetComponent<StudioEventEmitter>();
+        }
+
+        void FixedUpdate()
+        {
+            if (!emitter.IsPlaying()) emitter.Play();
+        }
+    }
+}
