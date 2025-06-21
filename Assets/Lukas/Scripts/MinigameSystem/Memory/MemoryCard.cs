558a1080b456a80e66d6e42ad8a31a69fea1925a
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2025-06-21 15:31:15 +0200

    Mini-Push
    - Framework for working on memory game

---
+++ Assets/Lukas/Scripts/MinigameSystem/Memory/MemoryCard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Scripts.MinigameSystem.Memory
+{
+    public class MemoryCard : MonoBehaviour, IPointerClickHandler
+    {
+        [field: SerializeField] public int Id {get; private set;}
+
+        public void TurnFaceUp()
+        {
+            
+        }
+        public void TurnFaceDown()
+        {
+            
+        }
+
+        public void RemoveFromBoard()
+        {
+            
+        }
+
+        public void OnPointerClick(PointerEventData _eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Lukas/Scripts/MinigameSystem/Memory/MemoryGame.cs
@@ -0,0 +1,25 @@
+﻿namespace Scripts.MinigameSystem.Memory
+{
+    public class MemoryGame : Minigame
+    {
+        public override void Play()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void EndGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OpenUI()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void CloseUI()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
