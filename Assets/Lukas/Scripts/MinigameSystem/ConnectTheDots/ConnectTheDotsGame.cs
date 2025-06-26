c12ca71f88316c797eeec15556b630b819c4bf95
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2025-06-26 17:27:44 +0200

    Update 0.0.4 - Part 3 - Minigame Update
    **Implementation**
    - Created the Connect the Dots game
    - Added TextMeshPro for UI
    - Created Utility Class for 2D Geometry based math and intersection of lines
    - Created placeholder prefab for lines

---
+++ Assets/Lukas/Scripts/MinigameSystem/ConnectTheDots/ConnectTheDotsGame.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Scripts.MinigameSystem.ConnectTheDots
+{
+    public class ConnectTheDotsGame : Minigame
+    {
+        [SerializeField] CanvasGroup minigameUIGroup;
+        [SerializeField] PlayerInput playerInput;
+        [SerializeField] List<Dot> dots = new List<Dot>();
+        UILineDrawer lineDrawer;
+        Dot selectedDot;
+
+        Dot startDot;
+
+        public struct LineSegment
+        {
+            public Vector2 start;
+            public Vector2 end;
+
+            public LineSegment(Vector2 _start, Vector2 _end)
+            {
+                start = _start;
+                end = _end;
+            }
+        }
+
+        readonly List<LineSegment> lineSegments = new List<LineSegment>();
+
+        void Awake()
+        {
+            lineDrawer = GetComponent<UILineDrawer>();
+        }
+
+        void OnEnable()
+        {
+            foreach (var dot in dots)
+            {
+                dot.OnClick += DotSelect;
+            }
+        }
+
+        void OnDisable()
+        {
+            foreach (var dot in dots)
+            {
+                dot.OnClick -= DotSelect;
+            }
+        }
+        
+        void DotSelect(Dot _dot)
+        {
+            if (selectedDot == null)
+            {
+                selectedDot = _dot;
+                Debug.Log("Selected Dot: " + selectedDot.name);
+                return;
+            }
+
+            Debug.Log("Selected Dot: " + selectedDot.name);
+
+            if (IsInvalidSelection(_dot)) return;
+
+            var start = selectedDot.GetComponent<RectTransform>().anchoredPosition;
+            var end = _dot.GetComponent<RectTransform>().anchoredPosition;
+            var newLineSegment = new LineSegment(start, end);
+
+            if (CreatesIntersection(newLineSegment))
+            {
+                Debug.Log("Intersection Detected!");
+                return;
+            }
+
+            if (IsTryingToReconnectToStart(_dot))
+            {
+                HandleWinningConnection(start, end);
+                return;
+            }
+
+            ConnectDot(start, end, newLineSegment, _dot);
+        }
+        
+        bool IsInvalidSelection(Dot _dot)
+        {
+            if (selectedDot.IsConnected) return true;
+            if (selectedDot == _dot) return true;
+
+            return _dot.IsConnected && !IsTryingToReconnectToStart(_dot);
+        }
+
+        bool IsTryingToReconnectToStart(Dot _dot)
+        {
+            return _dot == startDot && AllowConnectToFirstDot();
+        }
+
+        bool CreatesIntersection(LineSegment _newSegment)
+        {
+            return IntersectionUtility.IntersectAny(_newSegment, lineSegments);
+        }
+
+        void HandleWinningConnection(Vector2 _start, Vector2 _end)
+        {
+            lineDrawer.DrawLine(_start, _end);
+            Debug.Log("You won!");
+            EndGame();
+        }
+
+        void ConnectDot(Vector2 _start, Vector2 _end, LineSegment _segment, Dot _nextDot)
+        {
+            lineDrawer.DrawLine(_start, _end);
+            lineSegments.Add(_segment);
+            selectedDot.Connect();
+
+            if (startDot == null)
+                startDot = selectedDot;
+
+            selectedDot = _nextDot;
+        }
+
+        bool AllowConnectToFirstDot()
+        {
+            int unconnectedDots = dots.Count(_dot => _dot.IsConnected == false);
+            return unconnectedDots <= 1;
+        }
+
+        public override void Play()
+        {
+            OpenUI();
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            playerInput.enabled = false;
+        }
+
+        protected override void EndGame()
+        {
+            lineDrawer.ClearLinesFromScene();
+            lineSegments.Clear();
+            CloseUI();
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            playerInput.enabled = true;
+        }
+
+        //Function is used by unity button.
+        public void ResetGame()
+        {
+            lineDrawer.ClearLinesFromScene();
+            lineSegments.Clear();
+            startDot = null;
+            selectedDot = null;
+            foreach (var dot in dots)
+            {
+                dot.Disconnect();
+            }
+        }
+        
+        protected override void OpenUI()
+        {
+            minigameUIGroup.gameObject.SetActive(true);
+        }
+
+        protected override void CloseUI()
+        {
+            minigameUIGroup.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Lukas/Scripts/MinigameSystem/ConnectTheDots/IntersectionUtility.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Scripts.MinigameSystem.ConnectTheDots
+{
+    public static class IntersectionUtility
+    {
+
+        public static bool IntersectAny(ConnectTheDotsGame.LineSegment _newLine, List<ConnectTheDotsGame.LineSegment> _lineSegments)
+        {
+            return _lineSegments.Any(_line => DoLinesIntersect(_newLine.start, _newLine.end, _line.start, _line.end) && !IsSharedEndpoint(_newLine.start, _newLine.end, _line.start, _line.end));
+        }
+
+        static bool IsSharedEndpoint(Vector2 _p1, Vector2 _p2, Vector2 _p3, Vector2 _p4)
+        {
+            return NearlyEqual(_p1, _p3) || NearlyEqual(_p1, _p4) || NearlyEqual(_p2, _p3) || NearlyEqual(_p2, _p4);
+        }
+
+        static bool NearlyEqual(Vector2 _a, Vector2 _b, float _tolerance = 0.001f)
+        {
+            return Vector2.Distance(_a , _b) < _tolerance;
+        }
+
+        static bool DoLinesIntersect(Vector2 _p1, Vector2 _p2, Vector2 _p3, Vector2 _p4)
+        {
+            var a = _p2 - _p1;
+            var b = _p4 - _p3;
+            var c = _p3 - _p1;
+            
+            float denominator = Cross(a,b);
+            if (denominator == 0) return false;
+
+            float numerator1 = Cross(c,b);
+            float numerator2 = Cross(c,a);
+
+            float t1 = numerator1 / denominator;
+            float t2 = numerator2 / denominator;
+
+            return t1 is >= 0 and <= 1 && t2 is >= 0 and <= 1;
+        }
+
+        static float Cross(Vector2 _a, Vector2 _b)
+        {
+            return _a.x * _b.y - _a.y * _b.x;
+        }
+    }
+}
